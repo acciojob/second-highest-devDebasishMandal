b005ee6237bee70ee9d36a1895f8645d05eb9116
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2023-12-06 13:35:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+	function secondHighest(arr) {
+			//Write your code here
+      let mx=Number.MIN_VALUE,sm=Number.MIN_VALUE;
+      if(arr.length==0 || arr.length==1){
+        return -Infinity;
+      } else if(arr.length>1){
+        for(let i=0;i<arr.length;i++){
+          if(arr[i]>mx){
+             sm=mx;
+             mx=arr[i];
+             
+          }else if(arr[i]>sm && arr[i]!=mx){
+            sm=arr[i];
+          }
+        }
+      }
+      return sm === Number.MIN_VALUE ? -Infinity : sm;			
+		}
+
+		// function Main(){ 
+		// 	var n = prompt("Enter the number of elements");
+		// 	var arr = [];
+		// 	for (var i = 0; i < n; i++) {
+		// 		arr[i] = prompt("Enter element " + (i+1));
+		// 	}
+		// 	alert(secondHighest(arr));
+		// }
+		// Main();
